2a6c24d1f5f5c46d6c55cb758ed99a77a73ca52f
Zane D. R <26946175+TotallyAProgrammer@users.noreply.github.com>
2017-11-20 18:26:15 -0600

    Update perlprimenumbermather.pl

--- perlprimenumbermather.pl
+++ perlprimenumbermather.pl
@@ -2,30 +2,24 @@ use strict;
 use warnings;
 use Time::HiRes qw( time );
 
+my $foo;
+my $maximum;
+my $numbertotest;
+$maximum = 100000;
+
 my $start = time();
-my $i;
-my $o;
-my $e;
-my $j;
-my $prime_;
-my @prime_;
-my $p;
-$o = 2;
-$e = 10000;
 
-for($i=$o; $i<=$e; $i++){
-$p=0;
-    for($j=1; $j<=$i; $j++){
-        if($i % $j==0){
 
-            $prime_[$p] = "$j";
-            $p++;
+for ($numbertotest = 2; $numbertotest <= $maximum; $numbertotest++)
+{
+    for ($foo = $numbertotest / 2; $foo >= 2; $foo--)
+    {
+        if ($numbertotest % $foo == 0)
+        {
+            last;
         }
-        if ($prime_[1] == $i){
-        }
+
     }
-
 }
-
 my $end = time();
 printf("%.2f\n", $end - $start);
