c5343b7bb7e0cc07546f8f687d099400fe01eba9
Zane D. R <26946175+TotallyAProgrammer@users.noreply.github.com>
2017-11-20 18:25:36 -0600

    Update PythonPrimeNumbers.py

--- PythonPrimeNumbers.py
+++ PythonPrimeNumbers.py
@@ -3,11 +3,10 @@ maximum = 10000
 numbertotest = 2 
 
 start = time.time()
-for num in range(2,maximum):
-    prime = True
-    for i in range(2,num):
-        if (num%i==0):
-            prime = False
+for numbertotest in range(2,maximum):
+    for foo in range(numbertotest//2, 2, -1):
+        if (numbertotest%foo==0):
+            break
 
 end = time.time()
 print(end - start)
